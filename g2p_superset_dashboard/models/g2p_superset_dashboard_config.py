12673e84f899df34a5d6bba3e157625f389e9f6d
EyuaelB <eyuaelbezabeh@gmail.com>
2024-09-09 09:59:01 +0300

    added URL Validation for  dashboard

--- g2p_superset_dashboard/models/g2p_superset_dashboard_config.py
+++ g2p_superset_dashboard/models/g2p_superset_dashboard_config.py
@@ -1,4 +1,7 @@
+import re
+
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class SupersetDashboardConfig(models.Model):
@@ -6,12 +9,36 @@ class SupersetDashboardConfig(models.Model):
     _description = "Superset Dashboard Configuration"
 
     name = fields.Char(string="Dashboard name", required=True)
-    url = fields.Char(string="Dashboard URL", required=True)
+    url = fields.Char(
+        string="Dashboard URL",
+        required=True,
+        help="Enter the URL for your Superset dashboard. URL must start with 'http://' or 'https://'",
+    )
     group_name = fields.Char("Group name", required=True)
     group = fields.Many2one("res.groups")
     menu_name = fields.Char("Menu title", required=True)
     menu = fields.Many2one("ir.ui.menu")
     action = fields.Many2one("ir.actions.client")
+
+    @api.constrains("url")
+    def _oncreate_check_url(self):
+        for record in self:
+            record.check_url(record.url)
+
+    @api.onchange("url")
+    def _onchange_check_url(self):
+        for record in self:
+            if record.url:
+                record.check_url(record.url)
+
+    def check_url(self, url):
+        url_pattern = re.compile(
+            r"^(https?:\/\/)(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+$",
+            re.IGNORECASE,
+        )
+        if not url_pattern.match(url):
+            error_msg = "Invalid URL format. The URL must start with 'http://' or 'https://'."
+            raise ValidationError(error_msg)
 
     @api.model
     def create(self, vals):
